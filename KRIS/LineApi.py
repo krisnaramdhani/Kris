77345ab7fce718b279bbe09ea0e815a5b85b0e56
adekrisna <32455883+adekrisna@users.noreply.github.com>
2017-12-25 23:51:49 +0700

    Update LineApi.py

--- KRIS/LineApi.py
+++ KRIS/LineApi.py
@@ -42,9 +42,9 @@ class LINE:
     self.authToken = self.Talk.authToken
     self.cert = self.Talk.cert
     self._headers = {
-              'X-Line-Application': 'DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0', 
+              'X-Line-Application': 'CHROMEOS\t.1.4.5\tChrome_HP\t1',#'DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0', 
               'X-Line-Access': self.authToken, 
-              'User-Agent': 'Line/6.0.0 iPad4,1 9.0.2'
+              'User-Agent': 'Line/7.18.0'#'Line/7.18.0 AppleWebKit/534.30'#'Line/6.0.0 iPad4,1 9.0.2'#'Line/7.18.0 iPad4,1 9.0.2'
    }
     self.Poll = Poll(self.authToken)
     #self.channel = channel.Channel(self.authToken)
